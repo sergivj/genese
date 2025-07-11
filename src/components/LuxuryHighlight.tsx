59f6929ef64cd6cd59c029573654b0615e164988
sergivj <57764967+sergivj@users.noreply.github.com>
2025-07-12 01:07:38 +0200

    WIP: Color bg LuxuryHightlight

--- src/components/LuxuryHighlight.tsx
+++ src/components/LuxuryHighlight.tsx
@@ -6,10 +6,10 @@ import Image from 'next/image';
 
 export default function LuxuryHighlight() {
   return (
-    <div className="flex flex-col w-full font-cormorant bg-genese-black">
+    <div className="flex flex-col w-full font-cormorant bg-[#E0DAC8]">
       <motion.div className="w-full items-start justify-center bg-black/10 md:py-[2.5%] py-[1%]">
         <motion.h2
-          className="text-white text-4xl font-bold flex items-center justify-center gap-2"
+          className="text-black text-4xl font-bold flex items-center justify-center gap-2"
           initial={{ opacity: 0, y: 10 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.3 }}
@@ -20,13 +20,13 @@ export default function LuxuryHighlight() {
       </motion.div>
 
       <motion.div
-        className="w-full items-start justify-center bg-black/10 md:py-[2.5%] py-[1%]"
+        className="w-full items-start justify-center bg-black/10"
         initial={{ opacity: 0, y: 10 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 0.6 }}
         viewport={{ once: true, amount: 0.5 }}
       >
-        <div className="gap-10 w-full flex flex-col md:flex-row justify-center items-start px-6 md:px-20 py-12">
+        <div className="gap-10 w-full flex flex-col md:flex-row justify-center items-start px-6 md:px-20 py-4 pb-12">
           {/* Columna 1 */}
           <div className="w-full md:w-[400px]">
             <div className="relative w-full h-[500px] mb-4 overflow-hidden">
@@ -37,11 +37,11 @@ export default function LuxuryHighlight() {
                 className="object-cover"
               />
             </div>
-            <h3 className="text-xl font-bold uppercase mb-2 text-white">Bijoux de Genese</h3>
-            <p className="text-sm text-white mb-4">
+            <h3 className="text-xl font-bold uppercase mb-2 text-black">Bijoux de Genese</h3>
+            <p className="text-sm text-black mb-4">
               En 2025, Genese revolucionó la Alta Joyería con su colección más audaz e inspiradora.
             </p>
-            <a href="#" className="text-sm underline font-semibold hover:text-genese-gold transition text-white">DESCUBRIR</a>
+            <a href="#" className="text-sm underline font-semibold hover:text-genese-gold transition text-black">DESCUBRIR</a>
           </div>
 
           {/* Columna 2 */}
@@ -54,13 +54,13 @@ export default function LuxuryHighlight() {
                 className="object-cover"
               />
             </div>
-            <h3 className="text-xl font-bold uppercase mb-2 italic text-white">
+            <h3 className="text-xl font-bold uppercase mb-2 italic text-black">
               El savoir-faire
             </h3>
-            <p className="text-sm  mb-4 text-white">
+            <p className="text-sm  mb-4 text-black">
               La Alta Joyería Genese encarna la técnica y la tradición artesana con precisión atemporal.
             </p>
-            <a href="#" className="text-sm underline font-semibold hover:text-genese-gold transition text-white">DESCUBRIR</a>
+            <a href="#" className="text-sm underline font-semibold hover:text-genese-gold transition text-black">DESCUBRIR</a>
           </div>
         </div>
 
